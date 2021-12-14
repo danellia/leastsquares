25327736079118dbaa8a1527f714d11b30ead6d0
danellia <deshkadary2@gmail.com>
2021-12-14 15:31:47 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace leastsquares
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.googleSheetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.randomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.countToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.graph = new ZedGraph.ZedGraphControl();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.columnX = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.columnY = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.importToolStripMenuItem,
+            this.countToolStripMenuItem,
+            this.clearToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1133, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.googleSheetsToolStripMenuItem,
+            this.excelToolStripMenuItem,
+            this.randomToolStripMenuItem});
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.importToolStripMenuItem.Text = "Импорт";
+            // 
+            // googleSheetsToolStripMenuItem
+            // 
+            this.googleSheetsToolStripMenuItem.Name = "googleSheetsToolStripMenuItem";
+            this.googleSheetsToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.googleSheetsToolStripMenuItem.Text = "Google Sheets";
+            this.googleSheetsToolStripMenuItem.Click += new System.EventHandler(this.googleSheetsToolStripMenuItem_Click);
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.excelToolStripMenuItem.Text = "Excel";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // randomToolStripMenuItem
+            // 
+            this.randomToolStripMenuItem.Name = "randomToolStripMenuItem";
+            this.randomToolStripMenuItem.Size = new System.Drawing.Size(188, 26);
+            this.randomToolStripMenuItem.Text = "Рандом";
+            this.randomToolStripMenuItem.Click += new System.EventHandler(this.randomToolStripMenuItem_Click);
+            // 
+            // countToolStripMenuItem
+            // 
+            this.countToolStripMenuItem.Name = "countToolStripMenuItem";
+            this.countToolStripMenuItem.Size = new System.Drawing.Size(98, 24);
+            this.countToolStripMenuItem.Text = "Рассчитать";
+            this.countToolStripMenuItem.Click += new System.EventHandler(this.countToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(87, 24);
+            this.clearToolStripMenuItem.Text = "Очистить";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(67, 24);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // graph
+            // 
+            this.graph.Location = new System.Drawing.Point(266, 41);
+            this.graph.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.graph.Name = "graph";
+            this.graph.ScrollGrace = 0D;
+            this.graph.ScrollMaxX = 0D;
+            this.graph.ScrollMaxY = 0D;
+            this.graph.ScrollMaxY2 = 0D;
+            this.graph.ScrollMinX = 0D;
+            this.graph.ScrollMinY = 0D;
+            this.graph.ScrollMinY2 = 0D;
+            this.graph.Size = new System.Drawing.Size(850, 567);
+            this.graph.TabIndex = 11;
+            this.graph.UseExtendedPrintDialog = true;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.columnX,
+            this.columnY});
+            this.dataGridView.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 29;
+            this.dataGridView.Size = new System.Drawing.Size(234, 567);
+            this.dataGridView.TabIndex = 16;
+            // 
+            // columnX
+            // 
+            this.columnX.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.columnX.HeaderText = "x";
+            this.columnX.MinimumWidth = 90;
+            this.columnX.Name = "columnX";
+            this.columnX.Width = 90;
+            // 
+            // columnY
+            // 
+            this.columnY.HeaderText = "y";
+            this.columnY.MinimumWidth = 90;
+            this.columnY.Name = "columnY";
+            this.columnY.Width = 90;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1133, 619);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.graph);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Метод наименьших квадратов";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem googleSheetsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem randomToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem countToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private ZedGraph.ZedGraphControl graph;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnX;
+        private System.Windows.Forms.DataGridViewTextBoxColumn columnY;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,264 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using Excel = Microsoft.Office.Interop.Excel;
+using static System.Math;
+
+namespace leastsquares
+{
+    public partial class Form1 : Form
+    {
+        PointPairList pointPairs = new PointPairList();
+        PointPairList linear = new PointPairList();
+        PointPairList quadratic = new PointPairList();
+        double sumX, sumY, sumX2, sumXY, sumX2Y, sumX3, sumX4;
+        int count;
+        public Form1()
+        {
+            InitializeComponent();
+            graph.GraphPane.Title.Text = "";
+            graph.GraphPane.XAxis.Title.Text = "";
+            graph.GraphPane.YAxis.Title.Text = "";
+        }
+        #region data
+        private void getData()
+        {
+            pointPairs.Clear();
+            linear.Clear();
+            quadratic.Clear();
+            for (int row = 0; row < dataGridView.Rows.Count - 1; ++row)
+            {
+                PointPair pair = new PointPair(Convert.ToDouble(dataGridView.Rows[row].Cells["columnX"].Value), 
+                                               Convert.ToDouble(dataGridView.Rows[row].Cells["columnY"].Value));
+                pointPairs.Add(pair);
+            }
+            pointPairs.Sort();
+            graph.GraphPane.CurveList.Clear();
+            graph.AxisChange();
+            graph.Refresh();
+        }
+        private async void googleSheetsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.Rows.Clear();
+                await Task.Run(() =>
+                {
+                    GoogleCredential credential;
+                    string[] Scopes = { SheetsService.Scope.Spreadsheets };
+                    string credFile = "credentials.json";
+                    string spreadsheetId = "1rq_p7SH-JEcnRkFx6gfxTQGppG2x_ydCN5hKygSuILQ";
+                    string range = "Лист1!A:B";
+
+                    using (var fs = new FileStream(credFile, FileMode.Open, FileAccess.Read))
+                    {
+                        credential = GoogleCredential.FromStream(fs).CreateScoped(Scopes);
+                    }
+                    SheetsService service = new SheetsService(new Google.Apis.Services.BaseClientService.Initializer()
+                    {
+                        HttpClientInitializer = credential,
+                        ApplicationName = ""
+                    });
+                    var request = service.Spreadsheets.Values.Get(spreadsheetId, range);
+                    var values = request.Execute().Values;
+                    if (values != null && values.Count > 0)
+                    {
+                        foreach (var row in values)
+                        {
+                            dataGridView.Rows.Add(row[0].ToString(), row[1].ToString());
+                        }
+                        countToolStripMenuItem.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл пуст!");
+                    }
+                });
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+
+        }
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataGridView.Rows.Clear();
+                using (var ofd = new OpenFileDialog())
+                {
+                    ofd.DefaultExt = "*.xls;*.xlsx";
+                    ofd.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                    ofd.Title = "Выберите файл для импорта";
+                    if (ofd.ShowDialog() == DialogResult.OK && !string.IsNullOrWhiteSpace(ofd.FileName))
+                    {
+                        Excel.Application excelApp = new Excel.Application();
+                        Excel.Workbook workbook = excelApp.Workbooks.Open(ofd.FileName);
+                        Excel.Worksheet sheet = (Excel.Worksheet)workbook.Worksheets.get_Item(1);
+                        Excel.Range range = sheet.UsedRange;
+
+                        var lastCell = sheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);
+                        int lastColumn = lastCell.Column;
+                        int lastRow = lastCell.Row;
+                        for (int index = 0; index < lastRow; ++index)
+                        {
+                            dataGridView.Rows.Add(sheet.Cells[index + 1, 1].Text.ToString(), sheet.Cells[index + 1, 2].Text.ToString());
+                        }
+                        workbook.Close(false, Type.Missing, Type.Missing);
+                        excelApp.Quit();
+                        excelApp.Quit();
+                        GC.Collect();
+
+                        countToolStripMenuItem.Enabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл не выбран!");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+        private void randomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+            Random random = new Random();
+            int length = (int)random.Next(10, 40);
+            for (int index = 0; index < length; ++index)
+            {
+                dataGridView.Rows.Add(random.Next(0, 100), random.Next(0, 100));
+            }
+            countToolStripMenuItem.Enabled = true;
+        }
+        #endregion
+        #region buttons
+        private async void countToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                getData();
+                if (pointPairs.Count < 3)
+                {
+                    throw new Exception("Недостаточно точек!");
+                }
+                foreach (var pair in pointPairs)
+                {
+                    PointPairList point = new PointPairList();
+                    point.Add(pair);
+                    graph.GraphPane.AddCurve("", point, Color.Purple, SymbolType.Circle);
+                }
+                graph.AxisChange();
+                graph.Refresh();
+                count = pointPairs.Count;
+                countSum();
+                Task<string> linearFunc = makeLinearFunction();
+                Task<string> quadFunc = makeQuadraticFunction();
+                drawGraph(await linearFunc, linear, Color.DarkRed, SymbolType.None);
+                drawGraph(await quadFunc, quadratic, Color.DarkGreen, SymbolType.None);
+                //drawGraph(await quadFunc, pointPairs, Color.Transparent, SymbolType.None);
+                countToolStripMenuItem.Enabled = false;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Некорректные данные!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            graph.GraphPane.CurveList.Clear();
+            graph.Refresh();
+            dataGridView.Rows.Clear();
+            pointPairs.Clear();
+            linear.Clear();
+            quadratic.Clear();
+            sumX = sumY = sumX2 = sumXY = sumX2Y = sumX3 = sumX4 = count = 0;
+            countToolStripMenuItem.Enabled = true;
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        #endregion
+        #region visuals
+        private void drawGraph(string legend, PointPairList list, Color color, SymbolType type)
+        {
+            graph.GraphPane.AddCurve(legend, list, color, type);
+            graph.AxisChange();
+            graph.Refresh();
+        }
+        private Task<string> makeLinearFunction()
+        {
+            return Task.Run(() =>
+            {
+                double[] linArgs = linearModel(count);
+                return String.Format("y = {0}x + {1}", Round(linArgs[0], 4), Round(linArgs[1], 4));
+            });
+        }
+        private Task<string> makeQuadraticFunction()
+        {
+            return Task.Run(() =>
+            {
+                double[] quadArgs = quadraticModel(count);
+                return String.Format("y = {0}x^2 + {1}x + {2}", Round(quadArgs[0], 4), Round(quadArgs[1], 4), Round(quadArgs[2], 4));
+            });
+        }
+        #endregion
+        #region math
+        private void countSum()
+        {
+            foreach (var p in pointPairs)
+            {
+                sumX += p.X;
+                sumY += p.Y;
+                sumX2 += Pow(p.X, 2);
+                sumXY += p.X * p.Y;
+                sumX2Y += Pow(p.X, 2) * p.Y;
+                sumX3 += Math.Pow(p.X, 3);
+                sumX4 += Math.Pow(p.X, 4);
+            }
+        }
+        private double[] linearModel(int count)
+        {
+            double d = Pow(sumX, 2) - count * sumX2;
+            double b = (sumX * sumXY - sumX2 * sumY) / d;
+            double k = (sumX * sumY - count * sumXY) / d;
+
+            foreach (var p in pointPairs)
+            {
+                linear.Add(new PointPair(p.X, k * p.X + b));
+            }
+
+            return new double[] { k, b };
+        }
+        private double[] quadraticModel(int count)
+        {
+            double d = Round(count * sumX2 * sumX4 + 2 * (sumX2 * sumX * sumX3) - Pow(sumX2, 3) - Pow(sumX, 2) * sumX4 - count * Pow(sumX3, 2), 4); 
+            double a = Round((sumX2Y * sumX2 * count + 2 * (sumX3 * sumX * sumX2) - Pow(sumX2, 3) - Pow(sumX, 2) * sumX2Y - Pow(sumX3, 2) * count) / d, 4);
+            double b = Round((count * sumXY * sumX4 + sumX2 * sumX * sumX2Y + sumY * sumX3 * sumX2 - sumXY * Pow(sumX2, 2) - sumY * sumX * sumX4 - count * sumX3 * sumX2Y) / d, 4);
+            double c = Round((sumY * sumX2 * sumX4 + sumX2 * sumXY * sumX3 + sumX * sumX3 * sumX2Y - Pow(sumX2, 2) * sumX2Y - sumX * sumXY * sumX4 - sumY * Pow(sumX3, 2)) / d, 4);
+
+            foreach (var p in pointPairs)
+            {
+                quadratic.Add(new PointPair(p.X, a * Pow(p.X, 2) + b * p.X + c));
+            }
+
+            return new double[] { a, b, c };
+        }
+        #endregion
+    }
+}
